662405b3b986ac428267e0bc3223991d934435ec
Junior042 <eduardoalvesdeoliveirajunior@gmail.com>
2021-11-16 18:04:34 -0300

    fix alterações

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function list(){
     cont.innerHTML += `
     <div class="cont-linha-tarefa">
       <span class="cont-span ${teste[i].checked == true ? "checked" : ''}" id="cont-span${i}">
-        <div class="bolinha" onclick="risca(${i})"></div>
+        <div class="bolinha" id="bolinha${i}" onclick="risca(${i})"></div>
         <p>${teste[i].dado}</p>
       </span>
       <img src="./images/x.svg" alt="Close" onclick="del(${i})" class="img-close"/>
@@ -59,8 +59,30 @@ function del(dado){
 
 // Função que risca a linha com a tarefa respectiva 
 function risca(dado){
+  let bolinha = document.querySelector(`#bolinha${dado}`);
+
+
   arr = JSON.parse(localStorage.getItem('banco'))
   arr[dado].checked == false ? arr[dado].checked = true : arr[dado].checked = false
   localStorage.setItem('banco', JSON.stringify(arr))
   list()
+}
+
+// ================================================================================================
+let atv = false
+
+function light(){
+
+  if(atv == false){
+    let body = document.querySelector('body')
+    body.style.background = "white";
+    let input = document.querySelector('#entra-dados')
+    input.style.background = "white"
+    let cont_res = document.querySelector('.cont-res')
+    cont_res.style.background = "white"
+    let header = document.querySelector('header')
+    header.innerHTML = `
+    <img src="./images/bg-desktop-light.jpg" alt="">
+    `;
+  }
 }
